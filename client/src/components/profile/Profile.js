105f88250ca01b32e46683c41e9d01f5f9bf9789
MoltenPL85 <player454@mail.ru>
2018-06-19 19:18:40 +0500

    Added NotFound, Post, PostForm, ProfileAbout, ProfileCreds, ProfileGithub components

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -10,6 +10,12 @@ import Spinner from '../common/Spinner';
 import { getProfileByHandle } from '../../actions/profileActions';
 
 class Profile extends Component {
+  componentDidUpdate(prevProps) {
+    if (this.props.profile.profile === null && prevProps.profile.loading) {
+      this.props.history.push('/not-found');
+    }
+  }
+
   componentDidMount() {
     if (this.props.match.params.handle) {
       this.props.getProfileByHandle(this.props.match.params.handle);
@@ -34,9 +40,14 @@ class Profile extends Component {
             <div className="col-md-6" />
           </div>
           <ProfileHeader profile={profile} />
-          <ProfileAbout />
-          <ProfileCreds />
-          <ProfileGithub />
+          <ProfileAbout profile={profile} />
+          <ProfileCreds
+            education={profile.education}
+            experience={profile.experience}
+          />
+          {profile.githubusername ? (
+            <ProfileGithub username={profile.githubusername} />
+          ) : null}
         </div>
       );
     }
